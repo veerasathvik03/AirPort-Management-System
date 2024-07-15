c4a97d4789aa44deeddb03820aea643d332010e3
Veera Sathvik Kuppam <k.veerasathvik@gmail.com>
2024-07-15 08:11:10 +0530

    Removed unused code

--- AMS/src/main/java/com/Veera_Sathvik/AMS/exceptions/GlobalExceptionsClass.java
+++ AMS/src/main/java/com/Veera_Sathvik/AMS/exceptions/GlobalExceptionsClass.java
@@ -24,8 +24,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import org.springframework.web.context.request.WebRequest;
 
-import org.springframework.web.server.ResponseStatusException;
-
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
